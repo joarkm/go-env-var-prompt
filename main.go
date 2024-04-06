5e8739831665b376d82c76dfc8ae79e38d33edfb
Joar K. Mauseth <jkm99412@gmail.com>
2024-04-06 19:50:06 +0000

    Add fields for AccessTokenLifetimeInMinutes and SecurityTokenServiceUrl. Replace validation with validation library

--- main.go
+++ main.go
@@ -2,33 +2,52 @@ package main
 
 import (
 	"fmt"
-	"net/url"
 	"os"
-	"regexp"
+
+	"strconv"
 
 	"github.com/charmbracelet/huh"
+	"github.com/go-playground/validator/v10"
 )
 
 var (
-	DefaultHost              = "https://example.com"
-	FrontendVirtualDirectory string
-	DatabaseConnectionString string
-	exportEnv                bool
+	DefaultHost                  string
+	FrontendVirtualDirectory     string
+	DatabaseConnectionString     string
+	ExportEnv                    bool
+	AccessTokenLifetimeInMinutes string
+	SecurityTokenServiceUrl      string
 )
 
+// use a single instance of Validate, it caches struct info
+var validate validator.Validate
+
 func main() {
+
 	form := huh.NewForm(
 		huh.NewGroup(
 			huh.NewInput().
 				Title("Default Host").
+				Placeholder("example.com").
 				Value(&DefaultHost).
 				Validate(validateHost),
 			huh.NewInput().
 				Title("Frontend Virtual Directory").
-				Value(&FrontendVirtualDirectory),
+				Value(&FrontendVirtualDirectory).
+				Validate((validateVirtualDirectory)),
 			huh.NewInput().
 				Title("Database Connection String").
 				Value(&DatabaseConnectionString),
+			huh.NewInput().
+				Title("Url for security token service").
+				Placeholder("https://example.com").
+				Value(&SecurityTokenServiceUrl).
+				Validate((validateUrl)),
+			huh.NewInput().
+				Title("Access token lifetime in minutes").
+				Value(&AccessTokenLifetimeInMinutes).
+				Suggestions([]string{"3600"}).
+				Validate(validateAccessTokenLifetime),
 		),
 	)
 
@@ -41,40 +60,61 @@ func main() {
 		huh.NewGroup(
 			huh.NewConfirm().
 				Title("Export as environment variables?").
-				Value(&exportEnv),
+				Value(&ExportEnv),
 		),
 	)
 	fmt.Printf("Default Host: %s\n", DefaultHost)
 	fmt.Printf("Frontend Virtual Directory: %s\n", FrontendVirtualDirectory)
 	fmt.Printf("Database Connection String: %s\n", DatabaseConnectionString)
+	fmt.Printf("Access token lifetime in minutes: %s\n", AccessTokenLifetimeInMinutes)
+	fmt.Printf("Url for security token service: %s\n", SecurityTokenServiceUrl)
 
 	writeEnvForm.Run()
-	if exportEnv {
+	if ExportEnv {
 		os.Setenv("DEFAULT_HOST", DefaultHost)
 		os.Setenv("FRONTEND_VIRTUAL_DIR", FrontendVirtualDirectory)
 		os.Setenv("DB_CONNECTION_STRING", DatabaseConnectionString)
+		os.Setenv("ACCESS_TOKEN_LIFETIME_IN_MINUTES", AccessTokenLifetimeInMinutes)
+		os.Setenv("SECURITY_TOKEN_SERVICE_URL", SecurityTokenServiceUrl)
 		fmt.Println("Environment variables exported successfully.")
 	}
 }
 
+func validateAccessTokenLifetime(input string) error {
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	errs := formatPotentialValidationErrors(validate.Var(input, "required,number"))
+	if errs != nil {
+		return errs
+	}
+	// Input should now be safe to parse as a number
+	lifetime, errs := strconv.ParseInt(input, 10, 64)
+	if errs != nil {
+		return errs
+	}
+	return formatPotentialValidationErrors(validate.Var(lifetime, "gte=1"))
+}
+
+func validateUrl(input string) error {
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	return formatPotentialValidationErrors(validate.Var(input, "required,http_url"))
+}
+
+func validateVirtualDirectory(input string) error {
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	// No suitable validator found for virtual directories so went with the closest I could find
+	// TODO: Define custom validator
+	return formatPotentialValidationErrors(validate.Var(input, "required,alphanum"))
+}
+
 func validateHost(input string) error {
-	// Validate if the input is a valid URL with an optional port
-	u, err := url.Parse(input)
-	if err != nil {
-		return fmt.Errorf("invalid URL format")
-	}
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	return formatPotentialValidationErrors(validate.Var(input, "required,fqdn"))
+}
 
-	// Check if the host is not empty and has a valid format
-	if u.Host == "" {
-		return fmt.Errorf("host cannot be empty")
-	}
-
-	// Optional: Check if the port is valid (if provided)
-	portPattern := regexp.MustCompile(`^\d+$`)
-	if u.Port() != "" && !portPattern.MatchString(u.Port()) {
-		return fmt.Errorf("invalid port format")
+func formatPotentialValidationErrors(errs error) error {
+	if errs != nil {
+		return fmt.Errorf(errs.Error()) // output: Key: "" Error:Field validation for "" failed on the "email" tag
 	}
 
 	return nil
 }
-
